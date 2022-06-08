03118c2b5f1e48dc158e4926a0527e5f5163242a
sarambru <sarambiru@gmail.com>
2022-06-07 22:56:17 -0500

    minor homepage changes and styling fix for study page

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -5,7 +5,7 @@ import '..//../css/FlashcardForm.scss'
 const Home = () => (
   <>
     <h1> Create.Study.Learn</h1>
-    <p id='about'> Using flashcards to self-test helps you learn information, and also helps you to identify any weak spots you may have with the material.</p>
+    <p id='about'> Using flashcards to self-test helps you absorb information and identify any weak spots you may have with the material.</p>
     <p id='byline'>Sharpen your knowledge with Studium</p>
     <img src='https://media.istockphoto.com/vectors/abstract-global-network-connections-concept-design-with-light-bulb-vector-id1181249897?k=20&m=1181249897&s=612x612&w=0&h=Sp7oAF0qCzj6S6p-Zoqf_L58XEYDlcHCLNEJDkhIq0w=' alt='person sitting on couch using laptop' />
   </>
